e6d09b379ed07ba84535d48df27af753f097158b
duansongrong <1356275809@qq.com>
2018-03-13 12:44:13 +0800

    1

--- js/index.js
+++ js/index.js
@@ -85,7 +85,7 @@
 	}
 }
 
-//内容部分
+//搭配部分
 {	
 	function dapei(parent){
 	const types=parent.querySelectorAll(".type");
@@ -107,7 +107,7 @@
 	});
 }
 
-
+//推荐部分
 {
 	const btn1=document.querySelector(".star_btn3");
 	const btn2=document.querySelector(".star_btn4");
@@ -138,4 +138,91 @@
 		}
 		inner.style.marginLeft=-1226*n+"px";
 	}
+}
+
+//内容部分
+{	
+	function wheel(parent){
+		let prev=parent.querySelector(".neirong_btn");
+		let next=parent.querySelector(".neirong_btn2");
+		let inner=parent.querySelector(".neirong_inner");
+		let contents=parent.querySelectorAll(".neirong_ye");
+		let pagers=parent.querySelectorAll(".neirong_dian");
+		let n=0;
+		next.onclick=function(){
+			n++;
+			if(n===contents.length){
+				n=contents.length-1;
+				return;
+			}
+			inner.style.marginLeft=n*-296+"px";
+			pagers[n].classList.add("active");
+			pagers[n-1].classList.remove("active");
+			obj=pagers[n];
+		}
+		prev.onclick=function(){
+			n--;
+			if(n<0){
+				n=0;
+				return;
+			}
+			inner.style.marginLeft=n*-296+"px";
+			pagers[n].classList.add("active");
+			pagers[n+1].classList.remove("active");
+			obj=pagers[n];
+		}
+		let obj=pagers[0];
+		pagers.forEach(function(ele,index){
+			ele.onclick=function(){
+				obj.classList.remove("active");
+				ele.classList.add("active");
+				obj=ele;
+				inner.style.marginLeft=index*-296+"px";
+				n=index;
+			}
+		})
+	}
+	let items=document.querySelectorAll(".neirong_item");
+	items.forEach(function(ele){
+		wheel(ele);
+	})
+}
+
+//侧导航
+{
+	let labels=document.querySelectorAll(".banner_nav li");
+	let menus=document.querySelectorAll(".menu");
+	let obj=menus[0];
+	labels.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			menus[index].style.display="block";
+			obj=menus[index];
+		}
+		ele.onmouseleave=function(){
+			menus[index].style.display="none";
+		}
+	});
+}
+
+//xiala
+{
+	let box=document.querySelector(".nav_total");
+	let top=document.querySelector(".nav_xialawenzi");
+	let bottom=document.querySelector(".nav_xiala_bottom1");
+	let types=document.querySelectorAll(".nav_xialawenzi span");
+	let goodlist=document.querySelectorAll(".nav_goodlist");
+	top.onmouseenter=function(){
+		bottom.style.height="230px";
+	}
+	box.onmouseleave=function(){
+		bottom.style.height="0";
+	}
+	let obj=goodlist[0];
+	types.forEach(function(ele,index){
+		ele.onmouseenter=function(){
+			obj.style.display="none";
+			goodlist[index].style.display="block";
+			obj=goodlist[index];
+		}
+	});	
 }
